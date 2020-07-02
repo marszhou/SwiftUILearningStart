2125fedf702b6df8ee237507f2b7e52cab4f353f
matt zhou <matt@cruelyouth.com>
2020-07-02 13:02:32 +0800

    22.02. Layout and Stacks

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,13 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  var body: some View {
+    ZStack {
+      VStack {
+        Spacer()
+      }.frame(width: 300, height: 220)
+        .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/).cornerRadius(20).shadow(radius: 20).offset(x: 0, y: -20)
+      VStack {
+        HStack {
+          VStack(alignment: .leading) {
+            Text("UI 设计")
+              .font(.title)
+              .fontWeight(.semibold)
+              .foregroundColor(.white)
+            Text("证书")
+              .foregroundColor(Color("accent"))
+          }
+          Spacer()
+          Image("Logo1")
+        }
+        .padding(.horizontal, 20)
+        .padding(.top, 20)
+        Spacer()
+        Image("Card1")
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(width: 300, height: 110, alignment: .top)
+      }
+      .frame(width: 340.0, height: 220.0).background(Color.black).cornerRadius(20).shadow(radius: 20)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
