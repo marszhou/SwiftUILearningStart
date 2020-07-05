3bd158c125c3bdccf2d5bd8a495ad89ebeabbf89
matt zhou <matt@cruelyouth.com>
2020-07-05 16:47:12 +0800

    22.15. 3D Scroll Animation

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -28,8 +28,15 @@ struct HomeView: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 30) {
                     ForEach(sectionData) { item in
-                        SectionView(section: item)
+                        GeometryReader { geometry in
+                            SectionView(section: item)
+                            .rotation3DEffect(Angle(degrees:
+                                Double((geometry.frame(in: .global).minX - 30) / -15)
+                            ), axis: (x: 0, y: 10, z: 0))
+                        }
+                        .frame(width:275, height: 275)
                     }
+                    Spacer().frame(width: 10)
                 }
                 .padding(30)
                 .padding(.bottom, 30)
@@ -90,6 +97,6 @@ struct Section : Identifiable {
 
 let sectionData = [
     Section(title: "Prototype designs in SwiftUI", text: "18 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card6")), color: Color(#colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1))),
-    Section(title: "Build a SwiftUI app", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card3")), color: Color(#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1))),
-    Section(title: "SwiftUI Advanced", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card6")), color: Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
+    Section(title: "Build a SwiftUI app", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card3")), color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))),
+    Section(title: "SwiftUI Advanced", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card6")), color: Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)))
 ]
